640a23153342421860a31925ec0a5fba4f9b9b5c
Amato Luca <amatoroccoluca@gmail.com>
2024-11-01 13:18:50 +0100

    chore: add bg color to header

--- frontend/src/components/Header/Header.styled.ts
+++ frontend/src/components/Header/Header.styled.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-  background-color: #ffffff;
   padding-left: 24px;
   padding-right: 24px;
   padding-top: 12px;
   padding-bottom: 12px;
+  background-color: #f5f5f5;
 `;
