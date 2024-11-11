e3e7a1dee2718dab956b0e5c70d6d576c6e77a61
Amato Luca <amatoroccoluca@gmail.com>
2024-11-11 16:54:06 +0100

    chore: add color on hover to buttons

--- frontend/src/components/Button/StyledButton.ts
+++ frontend/src/components/Button/StyledButton.ts
@@ -13,20 +13,30 @@ export const StyledButton = styled.div<{ $inputColor: Color }>`
   justify-content: center;
   align-items: center;
   gap: 4px;
-  border: 1px solid #919191;
+  border: 1.5px solid #919191;
 
   &:hover {
-    background-color: #919191;
+    background-color: ${({ $inputColor }) => {
+      if ($inputColor === 'black') return ' #919191';
+      if ($inputColor === 'white') return '#ffffff';
+      return 'inherit'; // Valore di default se non corrisponde a black o white
+    }};
+
+    border-color: ${({ $inputColor }) => {
+      if ($inputColor === 'black') return '#919191';
+      if ($inputColor === 'white') return '#111111';
+      return 'inherit'; // Valore di default se non corrisponde a black o white
+    }};
+
     cursor: pointer;
   }
 
   svg {
     color: ${({ $inputColor }) => {
-      if ($inputColor === 'black') return '#ffffff ';
+      if ($inputColor === 'black') return '#ffffff';
       if ($inputColor === 'white') return '#111111';
       return 'inherit'; // Valore di default se non corrisponde a black o white
     }};
-
     font-size: 24px;
   }
 
@@ -39,6 +49,12 @@ export const StyledButton = styled.div<{ $inputColor: Color }>`
   color: ${({ $inputColor }) => {
     if ($inputColor === 'black') return '#ffffff ';
     if ($inputColor === 'white') return '#111111';
+    return 'inherit'; // Valore di default se non corrisponde a black o white
+  }};
+
+  border-color: ${({ $inputColor }) => {
+    if ($inputColor === 'black') return '#111111';
+    if ($inputColor === 'white') return '#919191';
     return 'inherit'; // Valore di default se non corrisponde a black o white
   }};
 `;
