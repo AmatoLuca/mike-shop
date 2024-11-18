b825865c8b26afe6bd741ffc017d7a94abedfd0f
Amato Luca <amatoroccoluca@gmail.com>
2024-11-18 18:46:02 +0100

    feat: add axios plugin in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import the plugin
+//import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    proxy: {
+      '/api': 'http://localhost:5001',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
+});
