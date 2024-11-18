36dc0f05ed8e5e4ab5c0f5c1c0e994175ee74bda
Amato Luca <amatoroccoluca@gmail.com>
2024-11-18 19:07:34 +0100

    chore: vite.config file update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import the plugin
-//import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
