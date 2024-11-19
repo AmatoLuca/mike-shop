518c0ef3be303feba2e6ae1721d3bbb104bba9e7
Amato Luca <amatoroccoluca@gmail.com>
2024-11-19 15:21:54 +0100

    feat: add mongoose models

---
+++ backend/models/productModel.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import Review from './reviewModel';
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    reviews: [Review],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
