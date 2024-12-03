4ec5e620903443935e3ed9211ac8978e9833cc1a
Amato Luca <amatoroccoluca@gmail.com>
2024-12-03 18:29:01 +0100

    refactor: remove useless code

--- frontend/src/screens/Product/ProductScreen.tsx
+++ frontend/src/screens/Product/ProductScreen.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo, useEffect } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import useShowMessage from '../../hooks/useShowMessage';
 import { StyledProductScreen } from './StyledProductScreen';
 import { useParams } from 'react-router-dom';
