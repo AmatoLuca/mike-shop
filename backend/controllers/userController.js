f78c1b6aa8f57e985b1a1d66e4e1229ebe9c2e53
Amato Luca <amatoroccoluca@gmail.com>
2024-12-10 16:20:44 +0100

    feat: add register user route logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
-
+import generateToken from '../utils/generateToken.js';
 /* 
    @desc    Auth user & get token
    @route   POST /api/users/login
@@ -9,22 +8,11 @@ import jwt from 'jsonwebtoken';
 */
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-
-    // Set JWT as HTTP-Only cookie
-    // We don't need to send back JWT to client each time we hit this route
-    // but automatically we are gonna ship the token true cookie (or just say, we store it in http cookie) http without storing it locally
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 1000, // 30 Days
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -44,7 +32,34 @@ const authUser = asyncHandler(async (req, res) => {
    @access  Public
 */
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 /* 
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  // Set JWT as an HTTP-Only cookie
+  // Set JWT as HTTP-Only cookie
+  // We don't need to send back JWT to client each time we hit this route
+  // but automatically we are gonna ship the token true cookie (or just say, we store it in http cookie) http without storing it locally
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  });
+};
+
+export default generateToken;
