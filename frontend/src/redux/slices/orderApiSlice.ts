a8424f9ebde77278f9579ad3821fa145a7b9a033
Amato Luca <amatoroccoluca@gmail.com>
2025-01-02 12:59:36 +0100

    refactor: payOrder slice

--- frontend/src/redux/slices/orderApiSlice.ts
+++ frontend/src/redux/slices/orderApiSlice.ts
@@ -19,7 +19,7 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
     }),
 
     payOrder: builder.mutation({
-      query: ({ orderId, details }: any) => ({
+      query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
         method: 'PUT',
         body: { ...details },
