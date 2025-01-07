4120141c43cbb94610ec992deba178bfa10a5a6e
Amato Luca <amatoroccoluca@gmail.com>
2025-01-07 13:56:54 +0100

    feat: [FE] Add logic to Content Profle component

--- frontend/src/components/Profile/Content/Content.tsx
+++ frontend/src/components/Profile/Content/Content.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useContext, useMemo } from 'react';
+import { ProfileContext } from '../Profile';
+import { TabOptionSelection } from '../models';
+import Account from '../Account/Account';
+import Orders from '../Orders/Orders';
 
 const Content = () => {
-  return <div>Content</div>;
+  const context = useContext(ProfileContext);
+
+  if (!context) {
+    throw new Error('YourComponent must be used within a ProfileProvider');
+  }
+
+  const profileContentToDisplay = useMemo(() => {
+    switch (context?.selectedTab) {
+      case TabOptionSelection.SETTINGS: {
+        return <Account />;
+      }
+
+      case TabOptionSelection.ORDERS: {
+        return <Orders />;
+      }
+
+      default:
+        return;
+    }
+  }, [context?.selectedTab]);
+
+  return <>{context && profileContentToDisplay}</>;
 };
 
 export default Content;
