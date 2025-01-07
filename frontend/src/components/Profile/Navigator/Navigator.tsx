6ac469e0be7d789c2340da59f56e985499700fdc
Amato Luca <amatoroccoluca@gmail.com>
2025-01-07 13:44:07 +0100

    refactor: [FE] refactor Navigator component into Profile folder component

--- frontend/src/components/Profile/Navigator/Navigator.tsx
+++ frontend/src/components/Profile/Navigator/Navigator.tsx
@@ -1,12 +1,38 @@
+import { useContext } from 'react';
+import { ProfileContext } from '../Profile';
 import { StyledNavigator } from './StyledNavigator';
 import { Link } from 'react-router-dom';
+import { TabOptionSelection } from '../models';
 
 const Navigator = () => {
+  const context = useContext(ProfileContext);
+
+  if (!context) {
+    throw new Error('YourComponent must be used within a ProfileProvider');
+  }
   return (
-    <StyledNavigator>
-      <Link to={''}>{'Settings'}</Link>
-      <Link to={''}>{'Orders'}</Link>
-    </StyledNavigator>
+    <>
+      {context && (
+        <StyledNavigator>
+          <Link
+            to={''}
+            onClick={() =>
+              context.tabSelectionHandler(TabOptionSelection.SETTINGS)
+            }
+          >
+            {TabOptionSelection.SETTINGS}
+          </Link>
+          <Link
+            to={''}
+            onClick={() =>
+              context.tabSelectionHandler(TabOptionSelection.ORDERS)
+            }
+          >
+            {TabOptionSelection.ORDERS}
+          </Link>
+        </StyledNavigator>
+      )}
+    </>
   );
 };
 
