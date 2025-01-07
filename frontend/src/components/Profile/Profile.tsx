59aad278125134b5625b5a3543dd1899633b1cf6
Amato Luca <amatoroccoluca@gmail.com>
2025-01-07 12:49:52 +0100

    feat: [FE] add Context tad other state to Profile scaffolding

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,12 +1,28 @@
+import { createContext, useState, useCallback } from 'react';
 import { StyledProfile } from './StyledProfile';
 import Navigator from './Navigator/Navigator';
+import Content from './Content/Content';
+import { TabOptionSelection } from './models';
 
-const Profile = () => {
+const Profile = ({ children }: { children: React.ReactNode }) => {
+  const ProfileContext = createContext({});
+
+  const [selectedTab, setSelectedTab] = useState<TabOptionSelection>(
+    TabOptionSelection.SETTINGS
+  );
+
+  const tabSelectionHandler = useCallback((selection: TabOptionSelection) => {
+    setSelectedTab(selection);
+  }, []);
+
   return (
-    <StyledProfile>
-      <Navigator />
-    </StyledProfile>
+    <ProfileContext.Provider value={{ selectedTab, tabSelectionHandler }}>
+      <StyledProfile>{children}</StyledProfile>
+    </ProfileContext.Provider>
   );
 };
+
+Profile.Navigator = Navigator;
+Profile.Content = Content;
 
 export default Profile;
