eab59fd71903f38022400cc9b4776f53bd90acf7
Amato Luca <amatoroccoluca@gmail.com>
2025-01-07 13:43:27 +0100

    feat: [FE] add  ProfileContextType type into Profile folder

--- frontend/src/components/Profile/models/index.ts
+++ frontend/src/components/Profile/models/index.ts
@@ -2,3 +2,8 @@ export enum TabOptionSelection {
   SETTINGS = 'Settings',
   ORDERS = 'Orders',
 }
+
+export type ProfileContextType = {
+  selectedTab: TabOptionSelection;
+  tabSelectionHandler: (selection: TabOptionSelection) => void;
+};
