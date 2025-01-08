501a17e7f56e148d3b962f1c728e4f620bbccd2f
Amato Luca <amatoroccoluca@gmail.com>
2025-01-08 17:27:34 +0100

    feat: [FE] add Orders component in Profile folder component

--- frontend/src/components/Profile/Orders/Orders.tsx
+++ frontend/src/components/Profile/Orders/Orders.tsx
@@ -1,7 +1,32 @@
 import { StyledOrders } from './StyledOrders';
+import { useGetMyOrdersQuery } from '../../../redux/slices/orderApiSlice';
+import { GetOrdersRequest } from '../../../redux/hooks';
+import { useMemo } from 'react';
+import Loader from '../../Loader/Loader';
+import Item from './Item/Item';
+import { Order } from '../../Order/models';
+import { OrderItem } from './models';
 
 const Orders = () => {
-  return <StyledOrders>Orders</StyledOrders>;
+  const {
+    data: orders,
+    isLoading,
+    isError,
+  }: GetOrdersRequest = useGetMyOrdersQuery({});
+
+  const completeOrdersList = useMemo(() => {
+    const orderItems = orders?.map((order: Order) => order.orderItems).flat();
+
+    return orderItems;
+  }, [orders]);
+
+  return (
+    <StyledOrders>
+      {completeOrdersList?.map((order: OrderItem) => {
+        return <Item orderData={order} key={order._id} />;
+      })}
+    </StyledOrders>
+  );
 };
 
 export default Orders;
