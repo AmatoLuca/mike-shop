00fec333756df6e080e1e1addfa32ac413cf8c8c
Amato Luca <amatoroccoluca@gmail.com>
2025-01-08 17:26:47 +0100

    style: [FE] add StyledItem

---
+++ frontend/src/components/Profile/Orders/Item/StyledItem.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledItem = styled.div`
+  color: rgb(17, 17, 17);
+  padding-bottom: 14px;
+  margin-bottom: 26px;
+  border-bottom: 1px solid #cfcfcf;
+  letter-spacing: 0.03rem;
+`;
