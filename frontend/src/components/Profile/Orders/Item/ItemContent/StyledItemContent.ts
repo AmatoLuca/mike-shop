51ecd5eb26d4133955cedd05ccfe50e74aef8cde
Amato Luca <amatoroccoluca@gmail.com>
2025-01-08 18:37:32 +0100

    style: [FE] add style to StyledItemContent

--- frontend/src/components/Profile/Orders/Item/ItemContent/StyledItemContent.ts
+++ frontend/src/components/Profile/Orders/Item/ItemContent/StyledItemContent.ts
@@ -32,5 +32,28 @@ export const StyledItemContent = styled.div`
       padding-bottom: 16px;
       font-weight: 600;
     }
+
+    .profile-orders-info-details-btn {
+      padding-top: 12px;
+
+      button {
+        font-size: 12px;
+        background-color: #111111;
+        border: 1px solid #111111;
+        color: #ffff;
+        padding-top: 6px;
+        padding-bottom: 6px;
+        padding-left: 12px;
+        padding-right: 12px;
+        border-radius: 30px;
+        letter-spacing: 0.03rem;
+
+        &:hover {
+          cursor: pointer;
+          background-color: #707072;
+          border-color: #707072;
+        }
+      }
+    }
   }
 `;
