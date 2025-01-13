5382c792324424bb7eec02dc46acef25b8019baa
Amato Luca <amatoroccoluca@gmail.com>
2025-01-13 16:35:43 +0100

    style: add StyledCreateReview

---
+++ frontend/src/components/Rating/Reviews/CreateReview/StyledCreateReview.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledCreateReview = styled.div`
+  padding-bottom: 30px;
+
+  .review-create {
+    width: 100px;
+    border-bottom: 1px solid rgb(17, 17, 17);
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    .review-create_action-title {
+      font-size: 14px;
+      letter-spacing: 0.02rem;
+    }
+  }
+`;
