13bbfeb938068905d2d36cbc4047d86869cb79ac
Amato Luca <amatoroccoluca@gmail.com>
2025-01-13 16:35:27 +0100

    refactor: [FE] review component

--- frontend/src/components/Rating/Reviews/Reviews.tsx
+++ frontend/src/components/Rating/Reviews/Reviews.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
 import { StyledReviews } from './Reviews.styled';
+import { useCreateReviewMutation } from '../../../redux/slices/productsApiSlice';
+import { CreateReviewRequest } from '../../../redux/hooks';
+import { GetUserInfo } from '../../../redux/selectors';
+import { ReviewsProps } from './models';
+import CreateReview from './CreateReview/CreateReview';
 
-const Reviews = () => {
-  return <StyledReviews>Review</StyledReviews>;
+const Reviews = ({ product }: ReviewsProps) => {
+  const UserInfoState = GetUserInfo();
+
+  const [
+    createReview,
+    { isLoading: isLoadingProductReview },
+  ]: CreateReviewRequest = useCreateReviewMutation();
+
+  return (
+    <StyledReviews>
+      {UserInfoState._id && <CreateReview />}
+      Review
+    </StyledReviews>
+  );
 };
 
 export default Reviews;
