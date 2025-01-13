b56f93d2bb4fede535b765fa18a0bb51f4a47d39
Amato Luca <amatoroccoluca@gmail.com>
2025-01-13 15:24:34 +0100

    feat: [BE] add reviews route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProductReview,
 } from '../controllers/productController.js';
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
+router.route('/:id/reviews').post(createProductReview);
 
 export default router;
