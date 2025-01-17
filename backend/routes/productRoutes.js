0dcbc32499c5a23bfb1398f4bc9268961f5775c5
Amato Luca <amatoroccoluca@gmail.com>
2025-01-17 15:14:56 +0100

    feat: [BE] sff ptotect route to reviews route controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,9 +5,10 @@ import {
   getProductById,
   createProductReview,
 } from '../controllers/productController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
 router.route('/:id').get(getProductById);
-router.route('/:id/reviews').post(createProductReview);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
