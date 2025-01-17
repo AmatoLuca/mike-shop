b92a767d83e71a20c59befadc0ce7ddb71bb3de4
Amato Luca <amatoroccoluca@gmail.com>
2025-01-17 15:15:38 +0100

    style: [FE] add Review styled component

--- frontend/src/components/Rating/Reviews/Reviews.styled.ts
+++ frontend/src/components/Rating/Reviews/Reviews.styled.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
 export const StyledReviews = styled.div`
-  padding-bottom: 30px;
+  .review-info-wrapper {
+    padding-top: 20px;
+    padding-bottom: 20px;
+
+    .review-info-header {
+      display: flex;
+      justify-content: space-between;
+      padding-right: 32px;
+
+      .review-info-header_info {
+        color: #707072;
+        padding-bottom: 16px;
+      }
+    }
+
+    .review-info-content {
+    }
+  }
 `;
