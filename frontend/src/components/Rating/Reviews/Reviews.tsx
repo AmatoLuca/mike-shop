56047ea715f657ea8a16085851e700f5cf6fcdfe
Amato Luca <amatoroccoluca@gmail.com>
2025-01-17 15:16:29 +0100

    feat: [FE] ultimate Reviews component

--- frontend/src/components/Rating/Reviews/Reviews.tsx
+++ frontend/src/components/Rating/Reviews/Reviews.tsx
@@ -1,22 +1,27 @@
 import { StyledReviews } from './Reviews.styled';
-import { useCreateReviewMutation } from '../../../redux/slices/productsApiSlice';
-import { CreateReviewRequest } from '../../../redux/hooks';
 import { GetUserInfo } from '../../../redux/selectors';
 import { ReviewsProps } from './models';
 import CreateReview from './CreateReview/CreateReview';
+import Stars from '../Stars/Stars';
 
 const Reviews = ({ product }: ReviewsProps) => {
   const UserInfoState = GetUserInfo();
 
-  const [
-    createReview,
-    { isLoading: isLoadingProductReview },
-  ]: CreateReviewRequest = useCreateReviewMutation();
-
   return (
     <StyledReviews>
       {UserInfoState._id && <CreateReview />}
-      Review
+      {product.reviews &&
+        product.reviews.map((review) => (
+          <div className="review-info-wrapper" key={review._id}>
+            <div className="review-info-header">
+              <Stars value={review.rating} />
+              <div className="review-info-header_info">{`${
+                review.name
+              } ${review.createdAt.substring(0, 9)}`}</div>
+            </div>
+            <div className="review-info-content">{review.comment}</div>
+          </div>
+        ))}
     </StyledReviews>
   );
 };
